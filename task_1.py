c718bc2c4ca7b15181b0e41ee252c1c1e58caa9f
Eugene-Fed <eugene.a.fed@gmail.com>
2022-11-30 20:12:30 +0600

    I checked several libraries for OCR. Either there were unsolvable problems during the installation process, or the result from using the library leaves much to be desired. Moved to the next library from the list in Google Spreadsheets.

--- task_1.py
+++ task_1.py
@@ -1,4 +1,18 @@
-import rupasportread as pr
+from doctr.models import ocr_predictor
+from doctr.io import DocumentFile
 
-pr.catching("pass_photos/0.jpeg")
+IMAGES = ['pass_photos/0.jpeg', 'pass_photos/1.jpeg', 'pass_photos/2.jpeg', 'pass_photos/3.jpeg',
+          'pass_photos/4.png', 'pass_photos/5.jpeg', 'pass_photos/6.jpeg', 'pass_photos/7.jpeg',
+          'pass_photos/8.jpeg', 'pass_photos/9.jpeg']
+model = ocr_predictor(det_arch='linknet_resnet18_rotation', reco_arch='master', pretrained=True)
+# model = ocr_predictor(pretrained=True)
+
+doc1 = DocumentFile.from_images(IMAGES[0])
+docs = DocumentFile.from_images(IMAGES)
+result = model(docs)
+result.show(docs)
+#print(result)
+
+
+# pr.catching("pass_photos/0.jpeg")
 # pr.download('pass_photos/4.png', '4.png')
