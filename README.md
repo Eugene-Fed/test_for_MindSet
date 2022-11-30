c3408c74e04b88dee0e1c42eb768949f0adb2baf
Eugene-Fed <eugene.a.fed@gmail.com>
2022-11-30 21:35:31 +0600

    TASK_1 is READY

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# test_for_MindSet
-Test work for an internship at MindSet
+## test_for_MindSet
+Test work for an internship at **MindSet**
+
+### TASK 1
+To run **task_1.py** you should add photos to directory **pass_photos/**
+Requires library preinstallation **PIL** and **Tesseract-OSR** with Russian *"traineddata"*
+
+Text recognition leaves much to be desired. But it turned out to be the easiest and most effective way of all that I tried before. Other libraries either do not recognize the Russian language at all, or had to face a huge number of problems during the installation process, or the volume of recognized text is even lower.
--- task_1.py
+++ task_1.py
@@ -1,18 +1,43 @@
-from doctr.models import ocr_predictor
-from doctr.io import DocumentFile
+from PIL import Image
+import pytesseract
+import os
 
-IMAGES = ['pass_photos/0.jpeg', 'pass_photos/1.jpeg', 'pass_photos/2.jpeg', 'pass_photos/3.jpeg',
-          'pass_photos/4.png', 'pass_photos/5.jpeg', 'pass_photos/6.jpeg', 'pass_photos/7.jpeg',
-          'pass_photos/8.jpeg', 'pass_photos/9.jpeg']
-model = ocr_predictor(det_arch='linknet_resnet18_rotation', reco_arch='master', pretrained=True)
-# model = ocr_predictor(pretrained=True)
-
-doc1 = DocumentFile.from_images(IMAGES[0])
-docs = DocumentFile.from_images(IMAGES)
-result = model(docs)
-result.show(docs)
-#print(result)
+DIR = 'pass_photos'                 # Path to the directory with photos
+LANG = 'rus'                        # Language of recognizing text
 
 
-# pr.catching("pass_photos/0.jpeg")
-# pr.download('pass_photos/4.png', '4.png')
+def get_files(directory: str) -> list:
+    """
+    Collecting a list of photo file names in work directory
+    :param directory: Path to the directory with photos
+    :return: List of file names
+    """
+    names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".png"):
+            names.append(os.path.join(directory, filename))
+
+    return names
+
+
+def recognize(img_path: str, lang: str) -> str:
+    """
+    Just recognizing text on selected files
+    :param img_path: Path to image
+    :param lang: Language of recognizing text
+    :return: All recognized text from image
+    """
+    text_data = f"========IMAGE '{img_path}' ========\n"           # Just for formatting
+    text_data = text_data + pytesseract.image_to_string(Image.open(img_path), lang=lang) + "\n"
+    print(text_data)
+    return text_data
+
+
+if __name__ == '__main__':
+    text = ""
+    images = get_files(directory=DIR)
+    for img in images:
+        text = text + recognize(img_path=img, lang=LANG)
+    print(f"Count of recognized files = {len(images)}")
+    with open("task_1.txt", 'w') as f:
+        f.write(text)
