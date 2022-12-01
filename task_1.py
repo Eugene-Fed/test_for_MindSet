14270434f43483158a6a3b6681f8fcff415f7ed2
Eugene-Fed <eugene.a.fed@gmail.com>
2022-12-01 21:04:31 +0600

    Added config for text detection and recognition

--- task_1.py
+++ task_1.py
@@ -4,6 +4,8 @@ import os
 
 DIR = 'pass_photos'                 # Path to the directory with photos
 LANG = 'rus'                        # Language of recognizing text
+PSM = 6                             # Page Segmentation Mode (--psm). Read more in Tesseract --help
+OEM = 3                             # Engine Mode (--oem). Read more in Tesseract --help
 
 
 def get_files(directory: str) -> list:
@@ -20,15 +22,18 @@ def get_files(directory: str) -> list:
     return names
 
 
-def recognize(img_path: str, lang: str) -> str:
+def recognize(img_path: str, lang: str, psm: int, oem: int) -> str:
     """
     Just recognizing text on selected files
     :param img_path: Path to image
     :param lang: Language of recognizing text
+    :param psm: Page Segmentation Mode (--psm). Read more in Tesseract --help
+    :param oem: Engine Mode (--oem). Read more in Tesseract --help
     :return: All recognized text from image
     """
+    config = f'--oem {oem} --psm {psm}'
     text_data = f"========IMAGE '{img_path}' ========\n"           # Just for formatting
-    text_data = text_data + pytesseract.image_to_string(Image.open(img_path), lang=lang) + "\n"
+    text_data = text_data + pytesseract.image_to_string(Image.open(img_path), lang=lang, config=config) + "\n"
     print(text_data)
     return text_data
 
@@ -37,7 +42,7 @@ if __name__ == '__main__':
     text = ""
     images = get_files(directory=DIR)
     for img in images:
-        text = text + recognize(img_path=img, lang=LANG)
+        text = text + recognize(img_path=img, lang=LANG, psm=PSM, oem=OEM)
     print(f"Count of recognized files = {len(images)}")
     with open("task_1.txt", 'w') as f:
         f.write(text)
